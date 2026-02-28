ee654e17c66c91f15842251554202835567c8141
Karlos Miguel Serrano <karlos_serrano2002@yahoo.com>
2026-02-28 16:50:09 +0800

    add commented SQL table schema for users

--- backend/server.ts
+++ backend/server.ts
@@ -29,3 +29,11 @@ app.use("/api/auth", authRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// CREATE TABLE users(
+// 	id SERIAL PRIMARY KEY,
+// 	name VARCHAR(100) NOT NULL,
+// 	email VARCHAR(100) UNIQUE NOT NULL,
+// 	password VARCHAR(255) NOT NULL,
+// 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+// )
